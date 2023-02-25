003bddb47df266017b5169e967816fe942e413a7
Moe007 <muhammed.moe.mi@gmail.com>
2023-02-25 19:34:14 +0200

    Remove search context

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,10 @@
 import { CartProvider } from "@/context/CartContext"
-import { SearchTermProvider } from "@/context/SearchTermContext"
 import "@/styles/globals.css"
 
 export default function App({ Component, pageProps }) {
 	return (
 		<CartProvider>
-			<SearchTermProvider>
-				<Component {...pageProps} />
-			</SearchTermProvider>
+			<Component {...pageProps} />
 		</CartProvider>
 	)
 }
